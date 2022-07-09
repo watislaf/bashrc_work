fdd32adfd7f20b7adea5cf6db965a79efbf0bff1
Vlad Kozulin <45079123+watislaf@users.noreply.github.com>
2022-07-09 14:58:42 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 function bash__remote_updater {
   BASH__REMOTE_UPDATER_DIRNAME=$(dirname -- "$0")
   function bash__remote_updater__init__ {
-    echo "source ${BASH__REMOTE_UPDATER_DIRNAME}/.bashrc" >>~./bashrc
+    echo "source ${BASH__REMOTE_UPDATER_DIRNAME}/bashrc_work.sh" >>~./bashrc
   }
   bash__remote_updater__init__
 }
