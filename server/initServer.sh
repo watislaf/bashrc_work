0a2959acec99815294ed543449a506326d212901
watislaf <vladkozulin@mail.ru>
2022-07-10 14:08:33 +0200

    changes

--- server/initServer.sh
+++ server/initServer.sh
@@ -1,8 +1,11 @@
 function bash__remote_updater__init__ {
-  BASH__REMOTE_UPDATER_DIRNAME="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-  cd $BASH__REMOTE_UPDATER_DIRNAME
+  BASH__REMOTE_UPDATER_DIRNAME="$(
+    cd -- "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
+  )"
+  cd ${BASH__REMOTE_UPDATER_DIRNAME}
   if [ $? != 0 ]; then
-  git pull
+    git pull
   fi
   cd -
   echo "export BASH__REMOTE_UPDATER_DIRNAME=${BASH__REMOTE_UPDATER_DIRNAME}" >>~/.bashrc
