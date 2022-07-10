bec39e92f43c8178f17c39ecf5e2631766de551c
watislaf <vladkozulin@mail.ru>
2022-07-10 12:07:58 +0200

    changes

--- server/initServer.sh
+++ server/initServer.sh
@@ -3,7 +3,7 @@ function bash__remote_updater__init__ {
   git pull
   echo "export BASH__REMOTE_UPDATER_DIRNAME=${BASH__REMOTE_UPDATER_DIRNAME}" >>~/.bashrc
   echo "source \${BASH__REMOTE_UPDATER_DIRNAME}/../src/bashrc_work.sh" >>~/.bashrc
+  source ~/.bashrc
 }
 
 bash__remote_updater__init__
-source ~/.bashrc
