dfbdc99e5063e1fb6d1f449ac1ab89a680802e0e
watislaf <vladkozulin@mail.ru>
2022-07-10 12:23:13 +0200

    changes

--- src/bashrc_work.sh
+++ src/bashrc_work.sh
@@ -324,8 +324,10 @@ function bash__remote_updater__ {
 
   echo "Update bashrc => ubr"
   function ubr {
-    cd "${BASH__REMOTE_UPDATER_DIRNAME}"
+    cd "${BASH__REMOTE_UPDATER_DIRNAME}/../"
     git pull
+    chmod +x ./server/autoPullDeamon.sh
+    chmod +x ./server/iniServer.sh
     cd -
     source ~/.bashrc
   }
