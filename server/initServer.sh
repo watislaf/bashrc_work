c8ed30b9ef4206cfe9673a1425ceeb23ce918ccc
watislaf <vladkozulin@mail.ru>
2022-07-10 14:09:35 +0200

    changes

--- server/initServer.sh
+++ server/initServer.sh
@@ -4,10 +4,11 @@ function bash__remote_updater__init__ {
     pwd -P
   )"
   cd ${BASH__REMOTE_UPDATER_DIRNAME}
-  if [ $? != 0 ]; then
-    git pull
+  if [ $? == 0 ]; then
+    echo error ${BASH__REMOTE_UPDATER_DIRNAME} is not a path
   fi
   cd -
+  git pull
   echo "export BASH__REMOTE_UPDATER_DIRNAME=${BASH__REMOTE_UPDATER_DIRNAME}" >>~/.bashrc
   echo "source \${BASH__REMOTE_UPDATER_DIRNAME}/src/bashrc_work.sh" >>~/.bashrc
   source ~/.bashrc
