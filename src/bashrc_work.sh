9ab144e8ba6f5d7e3bdfd41ddecee64a0552b99d
watislaf <vladkozulin@mail.ru>
2022-07-11 08:55:50 +0200

    changes

--- src/bashrc_work.sh
+++ src/bashrc_work.sh
@@ -52,6 +52,9 @@ function bash__decorations__ {
     git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
   }
   export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+(${debian_chroot})}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[\033[00;32m\]\$(git_branch)\[\033[00m\]\$ "
+  
+  # fzf
+  [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 }
 
 function bash__gTools__ {
