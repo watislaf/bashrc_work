e81bbeaf3b0829a280b22b903aeaff7b1f0eda94
watislaf <vladkozulin@mail.ru>
2022-07-12 18:03:32 +0200

    changes

--- src/bashrc_work.sh
+++ src/bashrc_work.sh
@@ -232,6 +232,7 @@ function bash__gTools__ {
 	  function gck() {
 	    ps aux | grep "/var/fpwork/kozulin/clion-2021.3/bin/clion.sh" | awk '{print $2;}' | xargs -I % -n 1 sh -c 'kill -9 %'
 	    ps aux | grep "/var/fpwork/kozulin/clion-2021.3/bin/fsnotifier" | awk '{print $2;}' | xargs -I % -n 1 sh -c 'kill -9 %'
+	    ps aux | grep "/var/fpwork/kozulin/clion-2021.3/jbr/bin/java" | awk '{print $2;}' | xargs -I % -n 1 sh -c 'kill -9 %'
 	  }
       
 	  echo "ClionStart => gsc"
