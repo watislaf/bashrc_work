3e80ddd4368aa1812f7a5184c7a4408304145524
watislaf <vladkozulin@mail.ru>
2022-07-30 18:31:51 +0200

    changes

--- src/bashrc_work.sh
+++ src/bashrc_work.sh
@@ -502,9 +502,12 @@ echo $1 | awk -F"$2" '{print$2}'
 		git diff --name-only |
 		egrep "(*.cpp$|*.hpp$|*.h$)" | 
 		egrep -v "/externals/integration/" |
+		egrep -v "/context/generator/" |
 		xargs -I % -n 1 bash -c "cut_preffix % $(cut_preffix $PWD /var/fpwork/${USER}/gnb/)" |
-		xargs -r  -I % -n 1 sh -c 'clang-format -i ./%  && echo "ClangFormat on ./%"'			
-        git add ./
+		xargs -r  -I % -n 1 sh -c 'clang-format -i ./%  && echo "Clang format on ./%"'
+		
+		git add ./ 
+
 		cd -
       }
 
